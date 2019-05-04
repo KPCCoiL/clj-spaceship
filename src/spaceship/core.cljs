4cdefeda0abc1e0390e149b2f7dbe4666d08d185
KPCCoiL <copernicium@me.com>
2019-05-04 21:35:34 +0900

    implement movement on computers

--- src/spaceship/core.cljs
+++ src/spaceship/core.cljs
@@ -3,24 +3,61 @@
    [goog.dom :as gdom]
    [rum.core :as rum]))
 
-(println "This text is printed from src/spaceship/core.cljs. Go ahead and edit it and see reloading in action.")
-
 (defn multiply [a b] (* a b))
 
-
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Here comes the game."}))
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
-(rum/defc hello < rum/reactive []
-  [:div
-   (:text (rum/react app-state))])
+(def width 200)
+(def height 400)
+(def plane-width 50)
+(def plane-height 50)
+(def speed 1)
+(defonce state
+  (atom {:y (/ height 2)
+         :moving false
+         :vy 0}))
+
+(defn next-frame [state]
+  (update state :y + (if (:moving state)
+                       (:vy state)
+                       0)))
+
+(defn update-vy [state mouse-event]
+  (assoc state :vy
+         (if (< (.-pageY mouse-event) (/ height 2))
+           (- speed)
+           speed)))
+
+(rum/defc screen < rum/reactive [state]
+  [:svg {:x "0px"
+         :y "0px"
+         :width (str width "px")
+         :height (str height "px")
+         :view-box (str "0 0 " width " " height)
+         :on-mouse-down (fn [ev]
+                          (swap! state assoc :moving true)
+                          (swap! state #(update-vy % ev)))
+         :on-mouse-move (fn [ev]
+                          (swap! state #(update-vy % ev)))
+         :on-mouse-up #(swap! state assoc :vy 0 :moving false)}
+   [:image {:href "/images/plane.svg"
+            :x (/ width 4)
+            :y (- (:y (rum/react state)) (/ plane-height 2))
+            :width plane-width
+            :height plane-height}]])
 
 (defn mount-app []
   (when-let [el (get-app-element)]
-    (rum/mount (hello) el)))
+    (rum/mount (screen state) el)))
+
+(mount-app)
+
+(defonce initialize-fps
+  (js/setInterval
+    #(swap! state next-frame)
+    (/ 1000 300)))
 
 ;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
