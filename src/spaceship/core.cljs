e248540875e7433a901e91515c884011fbc6d99e
KPCCoiL <copernicium@me.com>
2019-05-04 15:58:55 +0900

    deal with rum issue

--- src/spaceship/core.cljs
+++ src/spaceship/core.cljs
@@ -1,6 +1,7 @@
 (ns ^:figwheel-hooks spaceship.core
   (:require
-   [goog.dom :as gdom]))
+   [goog.dom :as gdom]
+   [rum.core :as rum]))
 
 (println "This text is printed from src/spaceship/core.cljs. Go ahead and edit it and see reloading in action.")
 
@@ -13,11 +14,18 @@
 (defn get-app-element []
   (gdom/getElement "app"))
 
+(rum/defc hello < rum/reactive []
+  [:div
+   (:text (rum/react app-state))])
 
+(defn mount-app []
+  (when-let [el (get-app-element)]
+    (rum/mount (hello) el)))
 
 ;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  (mount-app)
 )
